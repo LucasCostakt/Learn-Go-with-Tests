ae9f67be1dc27421c373cfeda5f0a66f0a3c5999
LucasCostakt <lucascosta.kt@gmail.com>
2020-09-02 12:00:15 -0300

    Concorrência
    Referência disponível em: https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/concorrencia

---
+++ concorrencia/verificaWebsites.go
@@ -0,0 +1,25 @@
+package concurrency
+
+type VerificadorWebsite func(string) bool
+type resultado struct {
+	string
+	bool
+}
+
+func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
+	resultados := make(map[string]bool)
+	canalResultado := make(chan resultado)
+
+	for _, url := range urls {
+		go func(u string) {
+			canalResultado <- resultado{u, vw(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		resultado := <-canalResultado
+		resultados[resultado.string] = resultado.bool
+	}
+
+	return resultados
+}
