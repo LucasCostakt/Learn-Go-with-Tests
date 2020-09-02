e8ab059c0d79dde7b1fc00591be5e0636dfd7e26
LucasCostakt <lucascosta.kt@gmail.com>
2020-09-02 12:04:38 -0300

    Contexto
    Refência dispónivel em: https://larien.gitbook.io/aprenda-go-com-testes/primeiros-passos-com-go/contexto

---
+++ contexto/server.go
@@ -0,0 +1,23 @@
+package contexto
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: registre o erro como você quiser
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
